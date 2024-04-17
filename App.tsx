1a4c952af152be1b214260dc10d453121f2f174b
Ugur-Atakan <uguratakansurmeli@gmail.com>
2024-04-17 17:04:23 +0300

    Add step counting module and dependencies, update AndroidManifest.xml and AppDelegate.h

--- App.tsx
+++ App.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, AppState, View} from 'react-native';
+import {
+  isStepCountingSupported,
+  startBackgroundService,
+  stopBackgroundService,
+  startStepCounterUpdate,
+  parseStepData,
+} from './src/lib/StepCounter';
 
 export default function App() {
-  const [stepCount, setStepCount] = React.useState(0);
+  const [stepCount, setStepCount] = useState(0);
+  const [stepCounterStatus, setStepCounterStatus] = useState({
+    supported: false,
+    granted: false,
+  });
+
+  AppState.addEventListener('change', handleAppStateChange);
+
+  function handleAppStateChange(nextAppState: string) {
+    if (nextAppState === 'background') {
+      startBackgroundService();
+    } else if (nextAppState === 'active') {
+      stopBackgroundService();
+    }
+  }
+  const checkStepCounterSupport = () => {
+    isStepCountingSupported().then(response => {
+      const {granted, supported} = response;
+      setStepCounterStatus({granted, supported});
+    });
+  };
+
+  const startStepCounter = () => {
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
+    startStepCounterUpdate(today, response => {
+      const data = parseStepData(response);
+      setStepCount(data.steps);
+    });
+  };
+  useEffect(() => {
+    checkStepCounterSupport();
+    startStepCounter();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Step count</Text>
-      <Text>{stepCount}</Text>
+      <Text>
+        Step Counter Destekleniyor mu?
+        {stepCounterStatus.supported ? 'Evet' : 'Hayır'}
+      </Text>
+      <Text>
+        Step Counter İzni verildi mi?
+        {stepCounterStatus.granted ? 'Evet' : 'Hayır'}
+      </Text>
+      <Text style={{fontSize: 24, color: 'black'}}>Step count</Text>
+      <Text style={{fontSize: 24, color: 'black'}}>{stepCount}</Text>
     </View>
   );
 }
--- android/app/src/main/java/com/stepcounting/services/BackgroundService.kt
+++ android/app/src/main/java/com/stepcounting/services/BackgroundService.kt
@@ -1,4 +1,46 @@
 package com.stepcounting.services
 
-class BackgroundService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class BackgroundService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startForeground(1, getNotification())
+        // Step counter başlatma kodları
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    private fun getNotification(): Notification {
+        val channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel("steps_channel", "My Background Service")
+        } else {
+            ""
+        }
+
+        return NotificationCompat.Builder(this, channelId)
+                .setContentTitle("Step Counter is running")
+                .setContentText("Step counting in background")
+                .setSmallIcon(androidx.core.R.drawable.notification_bg)
+                .build()
+    }
+
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW)
+            channel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+            getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
+        }
+        return channelId
+    }
 }
